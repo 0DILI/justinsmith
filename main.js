927a11faeff5944acc517c2329496318a937ead7
samuel <samuelodili12@gmail.com>
2025-04-25 23:15:45 +0100

    transition

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
+// let overlayActive = false;
+
+// function changeImage() {
+//   const img = document.getElementById("main-image");
+
+//   if (!overlayActive) {
+//     img.src = "assets/View5.png"; // Overlay image
+//   } else {
+//     img.src = "assets/Main-View.png"; // Original image
+//   }
+
+//   overlayActive = !overlayActive;
+// }
+
 let overlayActive = false;
 
 function changeImage() {
   const img = document.getElementById("main-image");
 
-  if (!overlayActive) {
-    img.src = "assets/View5.png"; // Overlay image
-  } else {
-    img.src = "assets/Main-View.png"; // Original image
-  }
+  img.classList.add("opacity-0");
 
-  overlayActive = !overlayActive;
+  setTimeout(() => {
+    img.src = overlayActive ? "assets/Main-View.png" : "assets/View5.png";
+    overlayActive = !overlayActive;
+
+    // Fade it back in
+    img.classList.remove("opacity-0");
+  }, 300); // Wait slightly less than the transition duration
 }
